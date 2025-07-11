37cfce8fb8729a01bebbdc5aaac2b1cf8e2e41b6
Tom Softreck <tom@sapletta.com>
2025-07-11 18:57:16 +0200

    update

--- pdf_processor/processing/__init__.py
+++ pdf_processor/processing/__init__.py
@@ -1,6 +1,6 @@
 """Core processing modules for PDF OCR."""
 
-from .pdf_processor import PDFOCRProcessor  # noqa
+from .pdf_processor import PDFProcessor  # noqa
 from .ocr_processor import OCRProcessor  # noqa
 from .image_enhancement import ImageEnhancer  # noqa
 from .svg_generator import SVGGenerator  # noqa
